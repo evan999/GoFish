0e2ecd37d4e967d9150206898e8dfc96fe9d2872
evan999 <emeshberge@gmail.com>
2020-12-12 23:12:40 -0500

    Building the GoFish objects

---
+++ src/com/cardgames/gofish/Card.java
@@ -0,0 +1,13 @@
+package com.cardgames.gofish;
+
+public class Card {
+    private int value;
+
+    public Card(){
+
+    }
+
+    public int getValue(){
+        return value;
+    }
+}
---
+++ src/com/cardgames/gofish/Deck.java
@@ -0,0 +1,8 @@
+package com.cardgames.gofish;
+
+import java.util.List;
+
+public class Deck {
+
+    private List<Card> cards;
+}
