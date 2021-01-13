c86298877bd75feef4012ec4f31f329ead0b59e8
evan999 <emeshberge@gmail.com>
2021-01-12 22:15:04 -0500

    Game update

--- src/com/cardgames/gofish/Game.java
+++ src/com/cardgames/gofish/Game.java
@@ -55,38 +55,56 @@ public class Game {
         }
     }
 
-    public void switchTurn(){
-        for(int count = 0; count < 50; count++){
-            System.out.println();
-        }
-    }
+//    public void switchTurn(){
+//        for(int count = 0; count < 50; count++){
+//            System.out.println();
+//        }
+//    }
 
-    public void request(int value){
-        // Player draws a card
-        boolean found = false;
+//    public boolean request(int value){
+//
+//        if(playerTurn){
+//
+//        }
+//    }
+
+//    public void getCardFromPlayer(){
+//        opponentHand.addCard(table.getPlayer().removeCard(index));
+//        index--;
+//    }
+
+
+
+    public boolean findMatch(int value){
 
         if(playerTurn){
             for (int index = 0; index < opponentHand.getCount(); index++){
                 if(opponentHand.getCardValue(index) == value) {
-                    found = true;
+//                    found = true;
                     playerHand.addCard(opponentHand.removeCard(index));
                     index--;
+                    return true;
                 }
             }
         }
         else{
             for (int index = 0; index < playerHand.getCount(); index++){
                 if(playerHand.getCardValue(index) == value) {
-                    found = true;
+//                found = true;
                     opponentHand.addCard(table.getPlayer().removeCard(index));
                     index--;
+                    return true;
                 }
             }
         }
 
-        if(!found && playerTurn){
-            System.out.println("\nGo Fish!");
+        return false;
+    }
+
+    public void fish(){
+        System.out.println("\nGo Fish!");
 //            table.getPlayer().findPairs();
+        if(playerTurn){
             playerTurn = false;
             if(!tableDeck.isEmpty()){
                 Card drawn = tableDeck.draw();
@@ -95,9 +113,7 @@ public class Game {
             }
             playerHand.findPairs();
         }
-        else if (!found && !playerTurn) {
-            System.out.println("\nGo Fish!");
-
+        else{
             playerTurn = true;
             if(!tableDeck.isEmpty()){
                 Card drawn = tableDeck.draw();
@@ -107,6 +123,60 @@ public class Game {
             opponentHand.findPairs();
         }
     }
+
+    public void request(int value){
+        findMatch(value);
+
+    }
+
+//    public void request(int value){
+//        // Player draws a card
+//        boolean found = false;
+//
+//        if(playerTurn){
+//            for (int index = 0; index < opponentHand.getCount(); index++){
+//                if(opponentHand.getCardValue(index) == value) {
+//                    found = true;
+//                    playerHand.addCard(opponentHand.removeCard(index));
+//                    index--;
+//                }
+//            }
+//        }
+//        else{
+//            for (int index = 0; index < playerHand.getCount(); index++){
+//                if(playerHand.getCardValue(index) == value) {
+//                    found = true;
+//                    opponentHand.addCard(table.getPlayer().removeCard(index));
+//                    index--;
+//                }
+//            }
+//        }
+//
+//        if(!found && playerTurn){
+//            System.out.println("\nGo Fish!");
+////            table.getPlayer().findPairs();
+//            playerTurn = false;
+//            if(!tableDeck.isEmpty()){
+//                Card drawn = tableDeck.draw();
+//                System.out.println("Card drawn: " + drawn);
+//                playerHand.addCard(drawn);
+//            }
+//            playerHand.findPairs();
+//        }
+//        else if (!found && !playerTurn) {
+//            System.out.println("\nGo Fish!");
+//
+//            playerTurn = true;
+//            if(!tableDeck.isEmpty()){
+//                Card drawn = tableDeck.draw();
+//                System.out.println("Card drawn: " + drawn);
+//                opponentHand.addCard(drawn);
+//            }
+//            opponentHand.findPairs();
+//        }
+//    }
+
+
 
     public boolean gameOver(){
         return playerHand.isEmpty() || opponentHand.isEmpty();
